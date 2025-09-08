77bd3ba764aec75c814b0e99eea6934b47f72239
asf <john.sakalli02@gmail.com>
2025-09-08 12:48:16 -0400

    overloaded the multiples methods to work with no input

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -16,4 +16,16 @@ public class Multiples {
         }
         return count;
     }
+
+    static int multiples() {
+        int i = 1;
+        int count = 0;
+        while (i < 1000){
+            if (i % 3 == 0 || i % 5 == 0){
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
 }
